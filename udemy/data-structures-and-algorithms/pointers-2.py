c19cae0c1aac0e2a58436d07b2deeaba948ad298
Pedro Henrique <phd19994@gmail.com>
2023-09-25 12:36:15 -0300

    feat:assets-folder

--- udemy/data-structures-and-algorithms/pointers-2.py
+++ udemy/data-structures-and-algorithms/pointers-2.py
@@ -4,7 +4,7 @@ dict_2 = dict_1
 print(f'dict_1 points to : {id(dict_1)}')
 print(f'dict_2 points to : {id(dict_2)}')
 
-dict_2 = 2
+dict_2['value'] = 2
 
 print(f'dict_1 points to : {id(dict_1)}')
 print(f'dict_2 points to : {id(dict_2)}')
